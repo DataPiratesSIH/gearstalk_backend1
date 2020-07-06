cc614147931dd117923edaf7e888d13222f71ccc
carol80 <carolseb80@gmail.com>
2020-07-06 13:39:48 +0530

    bug fixed

--- utils/rabbitmq.py
+++ utils/rabbitmq.py
@@ -31,24 +31,24 @@ def rabbitmq_bridge(data):
         Rabbitmq for realtime person detection
 ----------------------------------------------------'''
 
-# def rabbitmq_live(cam_id, lat, lng, url):
-#     credentials = pika.PlainCredentials('test', 'test')
+def rabbitmq_live(cam_id, lat, lng, url):
+    credentials = pika.PlainCredentials('test', 'test')
 
-#     connection = pika.BlockingConnection(
-#         pika.ConnectionParameters(host='127.0.1.1', credentials=credentials))
-#     channel = connection.channel()
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='127.0.1.1', credentials=credentials))
+    channel = connection.channel()
 
-#     channel.queue_declare(queue='live_data')
+    channel.queue_declare(queue='live_data')
 
-#     data = {
-#         "cam_id" : str(cam_id),
-#         "lat" : lat,
-#         "lng" : lng,
-#         "url" : url
-#     }
-#     message = json.dumps(data, ensure_ascii=False, indent=4)
-#     print(message)
+    data = {
+        "cam_id" : str(cam_id),
+        "lat" : lat,
+        "lng" : lng,
+        "url" : url
+    }
+    message = json.dumps(data, ensure_ascii=False, indent=4)
+    print(message)
 
-#     channel.basic_publish(exchange='', routing_key='live_data', body=message)
-#     print(" [x] Sent The JSON Data")
-#     connection.close()
+    channel.basic_publish(exchange='', routing_key='live_data', body=message)
+    print(" [x] Sent The JSON Data")
+    connection.close()
